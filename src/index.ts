a83482535d87dd1c0c30868479258ca595262850
Liam Eric Eim <eimliam28@gmail.com>
2025-08-23 21:30:18 +0200

    Add script 'start'

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,24 @@ client.once(Events.ClientReady, readyClient => {
 })
 
 client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+    if (!interaction.isChatInputCommand()) return
 
     console.log(interaction.commandName)
     const command = commands.get(interaction.commandName)
 
     if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
+        console.error(`No command matching ${interaction.commandName} was found.`)
+        return
     }
 
     try {
-        await command.execute(interaction);
+        await command.execute(interaction)
     } catch (error) {
-        console.error(error);
+        console.error(error)
         if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+            await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral })
         } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral })
         }
     }
 })
